6d07548888b902534b90633e8b5fbca650abf947
UBSITBeranAndrei <20243327@s.ubaguio.edu>
2026-04-16 18:49:39 +0800

    feat: generate Home, Learn, and About shell components with routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Learn } from './pages/learn/learn';
+import { About } from './pages/about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'learn', component: Learn },
+  { path: 'about', component: About },
+  { path: '**', redirectTo: '' }
+];
