74c3ec5db9a69429c6e7b756f466ff3fe7938ba7
UBSITBeranAndrei <20243327@s.ubaguio.edu>
2026-04-22 12:04:10 +0800

    feat: add responsive UI, SDG4 theme, navigation with RouterLinkActive, and global styles

---
+++ src/app/pages/resource-detail/resource-detail.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resource-detail',
+  imports: [],
+  templateUrl: './resource-detail.html',
+  styleUrl: './resource-detail.css',
+})
+export class ResourceDetail {}
